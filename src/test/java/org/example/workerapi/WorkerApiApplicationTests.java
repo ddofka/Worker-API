7dcd96090182ae87c1fa0e68e94beff22c4c0a56
ddofka <ddorofejevas@yahoo.com>
2025-03-18 21:57:53 +0200

    Initial commit

---
+++ src/test/java/org/example/workerapi/WorkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
